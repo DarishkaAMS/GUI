1eff771f3eec667e6ae42ef2b12a421a8cd379cf
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-12 07:41:44 +0200

    Add decoded_qr

--- QR-Code/qr_code.py
+++ QR-Code/qr_code.py
@@ -4,3 +4,6 @@ from PIL import Image
 
 qr_code = pyqrcode.create("Life is Amazing")
 qr_code.png("my_first_qr.png", scale=8)
+
+decoded_qr = decode(Image.open("my_first_qr.png"))
+print(decoded_qr[0].data.decode("ascii"))
