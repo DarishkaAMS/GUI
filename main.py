9322c90841b3f01769bfd2ddfcd529d69742317e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-12 06:37:20 +0200

    Created Pyhton Clock

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from time import strftime
 ROOT = Tk()
 ROOT.title("My first clock")
 
-label = Label(ROOT, font = )
+
+def time():
+    time_str = strftime('%H:%M:%S %p')
+    label.config(text=time_str)
+    label.after(1000, time) # 1 sec
 
 
+label = Label(ROOT, font=("ds-digital", 80), background="black", foreground="cyan")
+label.pack(anchor='center')
+
+time()
+mainloop() # to run the Tkinter event loop.
