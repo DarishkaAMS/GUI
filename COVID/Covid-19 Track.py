bb9bc24075a0abe0b88e7139f1b92651222103ac
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-28 10:39:21 +0200

    Add get_html_data & get_covid_data

--- COVID/Covid-19 Track.py
+++ COVID/Covid-19 Track.py
@@ -3,6 +3,16 @@ import bs4
 
 import tkinter as tk
 
+
 def get_html_data(url):
     data = requests.get(url)
     return data
+
+
+def get_covid_data():
+    url = "https://www.worldometers.info/coronavirus/"
+    html_data = get_html_data(url)
+    bs = bs4.BeautifulSoup(html_data.text, 'html.parser')
+    print(bs)
+
+get_covid_data()
